2304e6d45e4c419aa7150aff9bdd1795a2fa7763
Larry <user@Larry.local>
2025-05-03 23:53:02 +0000

    added buttons

--- To_do_List_Manager/main.py
+++ To_do_List_Manager/main.py
@@ -9,30 +9,30 @@ class Task:
     id=0
     def __init__(self,
         description:str,
-        due_date:dt.date,
+        due_date,
         status=False):
             self.description = description
             self.status = status
             self.due_date = due_date
             Task.id += 1
             self.id_new = Task.id
-           
+
     def is_completed(self):
         return 'completed' if self.status else 'pending..'
     def edit_description(self,text:str):
         self.description = text
         return self.description
     def get(self):
-        return  [self.id_new,self.description,self.status,self.due_date]
+        return  [self.id_new,self.description,self.is_completed(),self.due_date]
 
-dictionary = defaultdict(list)
+dictionary = defaultdict()
 
 class To_do_list:
     def __init__(self,task=dictionary):
         self.task = task
-    
-    def add_task(self,id,description,status,date):
-        self.task[id] = [description,status,date]
+
+    def add_task(self,task):
+        self.task[task[0]] = task
 
     def delete_task(self,id):
         del self.task[id]
@@ -42,15 +42,15 @@ class To_do_list:
         df = pd.DataFrame(data)
         df.columns = ['id','Task','status','date']
         return df
-        
+
     def get_pending(self,df):
         filtered = df[df['status'].str.lower()=='pending..']
         return  filtered
-        
+
     def  get_completed(self,df):
         filtered = df[df['status'].str.lower()=='completed']
         return  filtered
-        
+
     def  update(self,id,value):
         if isinstance(value,str):
             self.task[id][0] = value
@@ -66,4 +66,14 @@ class To_do_list:
 st.title('To_do_List Manager')
 st.sidebar.header('INPUTS')
 description = st.sidebar.text_input('Task')
+date = st.sidebar.date_input('Date')
+task = Task(description=description,due_date=date)
 
+button_view = st.sidebar.button('View')
+if  button_view:
+    st.header('New Task')
+    st.write(task.get())
+
+button_add = st.button('Add Task')
+lists =  To_do_list()
+if lists.add_task(task): st.markdown(':white_check_mark: Added')
