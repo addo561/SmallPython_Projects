44e1e2752ba53d5fcfe9cd7202ad9aedd9e17ba0
Larry <user@Larry.local>
2025-05-03 21:37:51 +0000

    Task class completed

--- To_do_List_Manager/main.py
+++ To_do_List_Manager/main.py
@@ -1,13 +1,11 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 from xmlrpc.client import DateTime
 
-#Class for each  task 
+#Class for each  task
 class Task:
     def __init__(self,
         description:str,
         status:str,
         due_date:DateTime):
             return {'Description':description,'Status':status,'Due_date':due_date}
-
-st.     
