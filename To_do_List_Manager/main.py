6a4bdd599a4f8415199d51a01ff9d06c55ae932a
Larry <user@Larry.local>
2025-05-03 21:38:49 +0000

    Task class completed

--- To_do_List_Manager/main.py
+++ To_do_List_Manager/main.py
@@ -1,3 +1,4 @@
+from jedi.inference.flow_analysis import Status
 import streamlit as st
 import pandas as pd
 from xmlrpc.client import DateTime
@@ -6,6 +7,16 @@ from xmlrpc.client import DateTime
 class Task:
     def __init__(self,
         description:str,
-        status:str,
-        due_date:DateTime):
-            return {'Description':description,'Status':status,'Due_date':due_date}
+        due_date:DateTime,
+        status=False,):
+            self.description = description
+            self.status = status
+            self.due_date = due_date
+    def is_completed(self):
+        return 'completed' if self.status else 'pending..'
+    def edit_description(self,text:str):
+        self.description = text
+        return self.description
+
+
+st.title('###To_do_List Manager')
